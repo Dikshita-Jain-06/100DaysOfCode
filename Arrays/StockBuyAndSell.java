c327b5e70635bef8d744da17b37051678e45235c
Dikshita Jain <jaindikshita234@gmail.com>
2026-06-14 23:32:58 +0530

    Day 2: buying and selling the stocks on different days when at most one transaction is allowed.

---
+++ Arrays/StockBuyAndSell.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // Code here
+        int n=prices.length;
+        int min = prices[0];
+        int res=0;
+        for(int i=1;i<n;i++){
+            min=Math.min(min, prices[i]);
+            res = Math.max(res,prices[i]-min);
+        }
+        return res;
+    }
+}
