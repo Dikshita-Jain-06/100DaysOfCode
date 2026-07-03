f1b44f96e4b4c69e2d66fd874db4b8ae6fb3fc7c
Dikshita Jain <jaindikshita234@gmail.com>
2026-07-03 20:36:55 +0530

    Day 14:find a subarray that has the largest product, and return the product.

---
+++ Arrays/MaximumProductSubarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int leftProduct = 1;
+        int rightProduct = 1;
+        int ans = nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(leftProduct==0) leftProduct=1;
+            if(rightProduct==0) rightProduct=1;
+            leftProduct = leftProduct*nums[i];
+            rightProduct = rightProduct*nums[n-1-i];
+            ans = Math.max(ans,Math.max(leftProduct,rightProduct));
+        }
+        return ans;
+    }
+}
