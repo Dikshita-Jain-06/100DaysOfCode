bab18ee94f7b2af4381a38bb4e4db6c59705efad
Dikshita Jain <jaindikshita234@gmail.com>
2026-07-25 16:15:10 +0530

    Day 33:every element appears twice except for one. Find that single one.

---
+++ Bit Manipulation/SingleNumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int xor=nums[0];
+        for(int i=1;i<nums.length;i++){
+            xor = xor^nums[i];
+        }
+        return xor;
+    }
+}
