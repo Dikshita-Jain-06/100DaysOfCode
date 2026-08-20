3824dbd5d0af0b6dda4f747da2423792a2ec90ca
Dikshita Jain <jaindikshita234@gmail.com>
2026-08-20 20:14:22 +0530

    Day 49:return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.

---
+++ Intervals/NonOverlappingIntervals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a,b) ->a[1]-b[1]);
+        int removals=0;
+        int prevEnd = intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if(prevEnd>start){
+                removals++;
+            }
+            else{
+                prevEnd = end;
+            }
+        }
+        return removals;
+    }
+}
