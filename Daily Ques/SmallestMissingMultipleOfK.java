561af38b9faffdc623ad3b63842169ab322b9adb
Dikshita Jain <jaindikshita234@gmail.com>
2026-08-25 23:13:13 +0530

    Day 53:return the smallest positive multiple of k that is missing from nums.

---
+++ Daily Ques/SmallestMissingMultipleOfK.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        int n=k;
+        while(set.contains(n)){
+            n+=k;
+        }
+        return n;
+    }
+}
