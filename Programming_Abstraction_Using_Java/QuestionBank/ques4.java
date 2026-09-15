7b541a52b81257b9273aad8369b144fdb5f4c2ab
Dikshita Jain <jaindikshita234@gmail.com>
2026-09-15 12:59:22 +0530

    Day 62:Convert Package Weights from Kilograms to Grams.

---
+++ Programming_Abstraction_Using_Java/QuestionBank/ques4.java
@@ -0,0 +1,45 @@
+// 4. Problem Statement 
+// A warehouse system stores package weights in an integer array. Due to a change in the 
+// measurement system, every package weight must be converted from kilograms to grams 
+// before generating the report. 
+// Write a Java program to read the package weights and display their corresponding values in 
+// grams. The calculation should use an appropriate data type because the converted value 
+// may exceed the normal integer range for larger inputs.
+
+// Input Format 
+// The first line contains an integer N. 
+// The second line contains N space-separated integers representing package weights in 
+// kilograms. 
+
+// Output Format 
+// Print the converted weights in grams separated by a single space. 
+
+// Code Constraints 
+// 1 ≤ N ≤ 1000 
+// 1 ≤ Weight ≤ 1000000 
+
+// Sample Input 
+// 4 
+// 12 25 40 8 
+
+// Sample Output 
+// 12000 25000 40000 8000
+import java.util.*;
+public class ques4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] arr = new long[n];
+        for(int i=0;i<n;i++){
+            int weight = sc.nextInt();
+            arr[i] = (long) weight*1000;
+
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+            if(i!=n-1){
+                System.out.print(" ");
+            }
+        }
+    }
+}
